e8f167fbbc5e42ed2b1ad21c8bd329eaa509fd37
sohaalsomairi <94217325+sohaalsomairi@users.noreply.github.com>
2021-11-14 09:27:27 +0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>إنشاء حساب جديد</title>
 
         <!-- Fonts -->
         <link href="/css/bootstrap.min.css" rel="stylesheet">
